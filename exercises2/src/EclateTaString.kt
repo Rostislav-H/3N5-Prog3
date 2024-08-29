2c91bbafff25e896fb125311ba6291fb313e5a7c
Rostislav Hudiakov <2043980@cegepmontpetit.ca>
2024-08-29 12:35:28 -0400

    [fct] Exercises de la session 2.1
    Completion des exercises 2.1

---
+++ exercises2/src/EclateTaString.kt
@@ -0,0 +1,14 @@
+
+fun stringed(name: String): String {
+    var gamin = ""
+    for (i in 1..name.length) {
+        gamin += "i"
+    }
+    return gamin
+
+}
+
+fun main() {
+
+       println(stringed("buh"))
+}
