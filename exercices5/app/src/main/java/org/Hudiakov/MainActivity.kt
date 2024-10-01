54b6d4703fe95a8f856b3da38ebe753ba21a3c13
rostislavH <2043980@cegepmontpetit.ca>
2024-10-01 09:38:32 -0400

    [FCT]Création des exercices 6.1
    Initialisation du fichier et création du projet

---
+++ exercices5/app/src/main/java/org/Hudiakov/MainActivity.kt
@@ -0,0 +1,22 @@
+package org.Hudiakov
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.depinfo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.i("Allo", "Message")
+    }
+}
