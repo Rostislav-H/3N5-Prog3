cc1ec374a12eee1465e090b331316a3fe84faf61
Rostislav Hudiakov <2043980@cegepmontpetit.ca>
2024-10-03 12:04:59 -0400

    [DEBUG] Ajout des fichiers manquants

---
+++ Exercices62/app/src/main/java/org/Hudiakov/Contact.kt
@@ -0,0 +1,21 @@
+package org.Hudiakov
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Contact : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        title = "Contact"
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_contact)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ Exercices62/app/src/main/java/org/Hudiakov/MainActivity.kt
@@ -0,0 +1,33 @@
+package org.Hudiakov
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        title = "Accueil"
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        binding.btnArticle.setOnClickListener {
+            val intent = Intent(this, Article::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+        }
+
+    }
+
+}
